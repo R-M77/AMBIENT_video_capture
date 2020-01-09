4b867080b5f2a1608407b2b0bad231d2d0b2668f
sam-osia <sam-osia>
2020-01-09 11:48:46 -0500

    finished webcam basic functionality

---
+++ webcam.py
@@ -0,0 +1,74 @@
+import cv2
+import pubsub
+from threading import Thread
+import numpy as np
+from datetime import datetime
+from pathlib import Path
+import os
+import time
+
+
+class Webcam:
+    def __init__(self, cam_id, display_image=True, timeout=30):
+        self.cam_id = cam_id
+        self.cap = cv2.VideoCapture(cam_id)
+        self.display_image = display_image
+        # set thread daemon to True to close when main code closes
+        self.t = Thread(target=self.threaded_capture, daemon=True)
+        self.frame = np.zeros((480, 640, 3), dtype='uint8')
+        self.vid_captures = {}
+        self.vid_cod = cv2.VideoWriter_fourcc(*'XVID')
+        self.parent_dir = r'D:\AMBIENT videos'
+        self.timeout = timeout
+
+    def begin(self):
+        self.t.start()
+
+    def threaded_capture(self):
+        while True:
+            ret, self.frame = self.cap.read()
+            print(self.frame.shape)
+            cv2.imshow(f'camera {self.cam_id}', self.frame)
+            if self.vid_captures:
+                for tag in self.vid_captures:
+                    self.vid_captures[tag].write(self.frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        self.cap.release()
+
+    def start_record(self, tag_id):
+        self.create_date_folder()
+        file_name = self.generate_file_path(tag_id)
+        output = cv2.VideoWriter(file_name, self.vid_cod, 30.0, (640, 480))
+        self.vid_captures[tag_id] = output
+        pass
+
+    def stop_record(self, tag_id):
+        try:
+            self.vid_captures[tag_id].release()
+            self.vid_captures.pop(tag_id, None)
+        except KeyError:
+            pass
+
+    def generate_file_path(self, tag_id):
+        folder_path = self.create_date_folder()
+        date_time_string = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+        file_name = f'{date_time_string}-ID-{tag_id}.avi'
+        file_path = os.path.join(folder_path, file_name)
+        return file_path
+
+    def create_date_folder(self):
+        date = datetime.now().strftime('%Y-%m-%d')
+        folder_path = os.path.join(self.parent_dir, date)
+        Path(folder_path).mkdir(parents=True, exist_ok=True)
+        return folder_path
+
+
+if __name__ == '__main__':
+    cam = Webcam(0)
+    cam.begin()
+    time.sleep(1)
+    cam.start_record(5)
+    time.sleep(60)
+    cam.stop_record(5)
